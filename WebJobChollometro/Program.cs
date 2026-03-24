74a15c4c1c7400ac71e1c2fdb324ffe2d2e73bbd
mmtol <mmigueltolosana@gmail.com>
2026-03-24 10:57:42 +0100

    aaa

--- WebJobChollometro/Program.cs
+++ WebJobChollometro/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebJobChollometro.Data;
+using WebJobChollometro.Repositories;
+
+//Console.WriteLine("Bienvenido a los chollos !!");
+
+string connString = "Data Source=sqlmaria3430.database.windows.net;Initial Catalog=AZURETAJAMAR;Persist Security Info=True;User ID=adminsql;Password=Admin123;Encrypt=True;Trust Server Certificate=True";
+
+//necesitamos la inyeccion de depdndencias
+var provider = new ServiceCollection()
+    .AddTransient<RepositoryChollometro>()
+    .AddDbContext<ChollometroContext>
+    (options => options.UseSqlServer(connString))
+    .BuildServiceProvider();
+
+//recuperamos el repo de la inyeccion
+RepositoryChollometro repo = provider.GetService<RepositoryChollometro>();
+
+//Console.WriteLine("Pulse Enter para comenzar ...");
+//Console.ReadLine();
+await repo.PopulateChollosAzureAsync();
+//Console.WriteLine("Proceso completado correctamente");
--- WebJobChollometro/Repositories/RepositoryChollometro.cs
+++ WebJobChollometro/Repositories/RepositoryChollometro.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Xml.Linq;
 using WebJobChollometro.Data;
+using WebJobChollometro.Models;
 
 namespace WebJobChollometro.Repositories
 {
@@ -22,6 +25,58 @@ namespace WebJobChollometro.Repositories
             {
                 return await context.Chollos.MaxAsync(x => x.IdChollo) + 1;
             }
+        }
+
+        public async Task<List<Chollo>> GetChollosWebAsync()
+        {
+            string url = "https://www.chollometro.com/rss";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Accept = @"text/html application/xhtml+xml, *.*";
+            request.Host = "www.chollometro.com";
+            request.Headers.Add("Accept-language", "es-ES");
+            request.Referer = "https://www.chollometro.com";
+            request.UserAgent = @"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)";
+
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            //este tipo de peticion se puede utilizar para todo
+            //podmeos leer un video, una imagen o simple texto html
+            //nos devuelve un stream
+            string xmlData = "";
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                xmlData = await reader.ReadToEndAsync();
+            }
+
+            XDocument doc = XDocument.Parse(xmlData);
+            var consulta = from datos in doc.Descendants("item")
+                           select datos;
+            int idChollo = await GetMaxIdCholloAsync();
+            List<Chollo> chollos = new List<Chollo>();
+            foreach (var tag in consulta)
+            {
+                Chollo c = new Chollo();
+                c.IdChollo = idChollo;
+                c.Titulo = tag.Element("title").Value;
+                c.Descripcion = tag.Element("description").Value;
+                c.Link = tag.Element("link").Value;
+                c.Fecha = DateTime.Now;
+                chollos.Add(c);
+                idChollo += 1;
+            }
+
+            return chollos;
+        }
+
+        public async Task PopulateChollosAzureAsync()
+        {
+            List<Chollo> chollos = await GetChollosWebAsync();
+
+            foreach (Chollo c in chollos)
+            {
+                await context.Chollos.AddAsync(c);
+            }
+            await context.SaveChangesAsync();
         }
     }
 }
