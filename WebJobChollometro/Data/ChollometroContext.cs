b943561e12f2a6def4d28fcc966b835fb00eb9d0
mmtol <mmigueltolosana@gmail.com>
2026-03-24 09:31:10 +0100

    Add project files.

---
+++ WebJobChollometro/Data/ChollometroContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebJobChollometro.Models;
+
+namespace WebJobChollometro.Data
+{
+    public class ChollometroContext : DbContext
+    {
+        public ChollometroContext(DbContextOptions<ChollometroContext> options) : base(options) { }
+        public DbSet<Chollo> Chollos { get; set; }
+    }
+}
---
+++ WebJobChollometro/Repositories/RepositoryChollometro.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebJobChollometro.Data;
+
+namespace WebJobChollometro.Repositories
+{
+    public class RepositoryChollometro
+    {
+        private ChollometroContext context;
+
+        public RepositoryChollometro(ChollometroContext context)
+        {
+            this.context = context;
+        }
+
+        private async Task<int> GetMaxIdCholloAsync()
+        {
+            if (context.Chollos.Count() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return await context.Chollos.MaxAsync(x => x.IdChollo) + 1;
+            }
+        }
+    }
+}
